33b870071429207485560fd48295b397a4e347ed
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-03-10 04:27:59 -0400

    functional omniauth for github

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310073803) do
+ActiveRecord::Schema.define(version: 20150310082023) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "task"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20150310073803) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
